7e6008d7643867aafbc3df84b41523147b0a7b70
Salvatore Skare <salskare@gmail.com>
2024-09-14 10:38:02 -0500

    Add defn for new gecko enclosure

---
+++ gecks/gecks.ino
@@ -0,0 +1,50 @@
+#include <VivariumMonitor.h>
+#include <PIDController.h>
+#include <ESP8266WiFi.h>
+
+// Common handler functions
+#include <common_functions.h>
+
+VivariumMonitor monitor;
+PIDController heat_controller(33.0, 4.0, 0, 1.2, 0.6);
+
+void setup() {
+  DEBUG_MSG("Vivarium Monitor firmware " FIRMWARE_VERSION);
+  Url stats_url = {
+    {.host = "stats.salskare.ml"},      
+    {.path = "/"},
+    .port = 5000,
+    .set = true,
+  };
+
+  VivariumMonitorConfig config = {
+    .has_sht_sensor = true,
+    .num_therm_sensors = 2,
+    .sample_interval = 10,
+    .ntp_zone = "CST6CDT,M3.2.0,M11.1.0",
+    .ntp_server = "pool.ntp.org",
+    .stats_url = stats_url,
+    .stats_interval = 300,
+  };
+
+  // Set hostname
+  WiFi.hostname(F("viv-monitor-gecks"));
+  // Set output handlers
+  monitor.setDigitalOneHandler(twelve_hour_on);
+  monitor.setAnalogHandler(analog_handler);
+
+  // Initialize hardware
+  monitor.init(config);
+}
+
+void loop() {
+  monitor.handle_events();
+}
+
+/*
+ * Handler for deep heat projector
+ */
+byte analog_handler(SensorData reading, time_t now)
+{
+  return heat_controller.add_reading(reading.high_temp, reading.timestamp);
+}
